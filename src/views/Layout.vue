ba194dd2ff27577823fe9beb89c025cb215a1823
lhw <lhw941823@gmail.com>
2022-11-07 23:35:12 +0800

    🎉 INT COMMIT: 22

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,23 +1,32 @@
 <template>
-    <!-- 顶部通栏 -->
-    <nav>顶部通栏</nav>
-    <!-- 头部组件 -->
-    <header>头部组件</header>
-    <!-- 内容容器 -->
-    <div class="main">
-        <!-- 二级路由 -->
-        <RouterView />
-    </div>
-    <!-- 底部组件 -->
-    <footer>底部组件</footer>
+  <!-- 顶部通栏 -->
+  <AppNavbar></AppNavbar>
+
+  <!-- 头部组件 -->
+  <AppHeader></AppHeader>
+  <!-- 内容容器 -->
+  <div class="app-body">
+    <!-- 二级路由 -->
+    <RouterView />
+  </div>
+  <!-- 底部组件 -->
+  <AppFooter></AppFooter>
 </template>
 
 <script>
+import AppNavbar from '../components/app-navbar.vue'
+import AppHeader from '../components/app-header.vue'
+import AppFooter from '../components/app-footer.vue'
+
 export default {
+
+  components: { AppNavbar, AppHeader, AppFooter },
   name: 'Layout'
 }
 </script>
 
 <style scoped lang="less">
-
+.app-body {
+  min-height: 600px;
+}
 </style>
