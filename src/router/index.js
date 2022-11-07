c85e8bf9c9a800436d0b348cae093b7174f5990f
lhw <lhw941823@gmail.com>
2022-11-07 17:51:24 +0800

    🎉 INT COMMIT: 修改

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+const Layout = () => import('@/views/Layout')
+const Home = () => import('@/views/home')
+
+// 路由规则
+const routes = [
+  // 一级路由布局容器
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: Home
+      }
+    ]
+  }
+
+]
+
+const router = createRouter({
+  // 路由模式
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        HomePage
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped lang="less">
+@import '@/assets/style/mixins.less';
+
+.container {
+    .hoverShadow()
+}
+</style>
